862e419f86b7d65d8e434e7c6d269990e0166da7
ishiIIT <ishitagoyal136@gmail.com>
2023-12-30 13:03:42 +0530

    dummy

---
+++ example.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        string c = "abcdefgh";
+        string r = "12345678";
+        for(int i=0;i<8;i++)
+        {
+            if(c[i]!=s[0])
+            cout<<c[i]<<s[1]<<endl;
+            if(r[i]!=s[1])
+            cout<<s[0]<<r[i]<<endl;
+        } 
+    }
+}
